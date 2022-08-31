25abaedd1bd4da963b8a1f1eaf4c47afa74b283f
Teresia Mrema-Buza <ndelly@gmail.com>
2022-08-30 21:16:27 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,7 +6,7 @@ st.write("""
   This is just a test for some of the amazing things done by stremlit.
   """)
 
-st.write(m.run(window.run = 15))
+st.write(m.run(window = 15))
 
 window = st.slider("Forecast window (days)")
 st.write(m.run(window = window))
